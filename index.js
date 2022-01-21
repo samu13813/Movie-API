5aa6ab4017ac83c6396658e479c267a36ab1cdf0
Sam Garcia <samuel.gsuero@gmail.com>
2022-01-21 16:16:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-console.log('Hello World');
+const express = require("express"),
+  app = express(),
+  http = require("http"),
+  url = require("url");
+
+let topBooks = [
+  {
+    title: "Interstellar",
+    author: "Christopher Nolan"
+  },
+  {
+    title: "Django Unchained",
+    author: "Quentin Tarantino"
+  },
+  {
+    title: "Avengers: Infinity War",
+    author: "Anthony Russo; Joe Russo"
+  },
+  {
+    title: "Guardians of the Galaxy",
+    author: "James Gunn"
+  },
+  {
+    title: "Dune",
+    author: "Denis Vulleneuve"
+  },
+  {
+    title: "Red Notice",
+    author: "Rawson Marshall Thurber"
+  },
+  {
+    title: "Pulp Fiction",
+    author: "Quentin Tarantino"
+  },
+  {
+    title: "Joker",
+    author: "Todd Phillips"
+  },
+  {
+    title: "John Wick",
+    author: "Chad Stahelski"
+  },
+  {
+    title: "The Dark Knight",
+    author: "Christopher Nolan"
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my Movies API!");
+});
+
+// app.get("/documentation", (req, res) => {
+//   res.sendFile("/public/documentation.html", {root: __dirname});
+// });
+
+app.use("/documentation", express.static('public'));
+
+
+app.get("/books", (req, res) => {
+  res.json(topBooks);
+});
+
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.")
+});
