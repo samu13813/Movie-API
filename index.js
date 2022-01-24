2e656689404bb50cd7cb4aa75a46237d8287eecc
Sam Garcia <samuel.gsuero@gmail.com>
2022-01-24 10:44:52 +0100

    documentation updated

--- index.js
+++ index.js
@@ -63,6 +63,11 @@ app.get("/books", (req, res) => {
   res.json(topBooks);
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.")
 });
