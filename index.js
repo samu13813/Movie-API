ee663760436e2e5ae07c6e66bc3d4fd544e7b807
Sam Garcia <samuel.gsuero@gmail.com>
2022-02-02 18:06:04 +0100

    syntax updated to mongoose and tested with no bugs

--- index.js
+++ index.js
@@ -24,160 +24,76 @@ app.use(morgan("common"));
 app.use(express.static('public'));
 
 
-let users = [
-  {
-    id: 1,
-    name: "Ethan",
-    favoriteMovies: ["Django Unchained"]
-  },
-  {
-    id: 2,
-    name: "Marshall",
-    favoriteMovies: []
-  }
-];
-
-let movies = [
-  {
-    id: 1,
-    title: "Interstellar",
-    genre: "Science fiction",
-    director: {
-      name: "Christopher Nolan",
-      dob: "30/07/1970"
-    }
-  },
-  {
-    id: 2,
-    title: "Django Unchained",
-    genre: "Western",
-    director: {
-      name: "Quentin Tarantino",
-      dob: "27/03/1963"
-    }
-  },
-  {
-    id: 3,
-    title: "Avengers: Infinity War",
-    genre: "Action",
-    director: {
-      name: "Russo Brothers",
-      dob: "03/02/1970 - 18/07/1971 "
-    }
-  },
-  {
-    id: 4,
-    title: "Guardians of the Galaxy",
-    genre: "Action",
-    director: {
-      name: "James Gunn",
-      dob: "05/08/1966"
-    }
-  },
-  {
-    id: 5,
-    title: "Dune",
-    genre: "Science fiction",
-    director: {
-      name: "Denis Vulleneuve",
-      dob: "03/08/1967"
-    }
-  },
-  {
-    id: 6,
-    title: "Red Notice",
-    genre: "Comedy",
-    director: {
-      name: "Rawson Marshall Thurber",
-      dob: "09/02/1975"
-    }
-  },
-  {
-    id: 7,
-    title: "Pulp Fiction",
-    genre: "Crime",
-    director: {
-      name: "Quentin Tarantino",
-      dob: "27/03/1963"
-    }
-  },
-  {
-    id: 8,
-    title: "Joker",
-    genre: "Crime",
-    director: {
-      name: "Todd Phillips",
-      dob: "20/12/1970"
-    }
-  },
-  {
-    id: 9,
-    title: "John Wick",
-    genre: "Action",
-    director: {
-      name: "Chad Stahelski",
-      dob: "20/09/1968"
-    }
-  },
-  {
-    id: 10,
-    title: "Fury",
-    genre: "War",
-    director: {
-      name: "David Ayer",
-      dob: "18/01/1968"
-    }
-  }
-];
-
-// Gets the list of data about ALL movies
+// Get all movies
 
 app.get("/movies", (req, res) => {
-  res.status(200).json(movies);
+  Movies.find()
+    .then((movies) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
-// Gets the data about a single movie, by name
+// Get movie by title
 
-app.get("/movies/:title", (req, res) => {
-  const movie = movies.find(movie => movie.title === req.params.title);
-
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send("Movie with title " + req.params.title + " was not found.");
-  }
+app.get("/movies/:Title", (req,res) => {
+  Movies.findOne({ Title: req.params.Title})
+    .then((movie) => {
+      if(movie){
+        res.status(200).json(movie);
+      } else {
+        res.status(400).send(req.params.Title + " was not found.")
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 // Gets the data about a genre, by name
 
-app.get("/movies/genre/:genreName", (req,res) => {
-  const genre = movies.find(movie => movie.genre === req.params.genreName).genre;
-
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send("Genre with name " + req.params.genreName + " was not found.")
-  }
+app.get("/movies/genre/:GenreName", (req, res) => {
+  Movies.findOne({ "Genre.Name" : req.params.GenreName})
+  .then((genre) => {
+    if(genre) {
+      res.status(200).json(genre.Genre);
+    } else {
+      res.status(400).send(req.params.GenreName + " was not found.")
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
 });
 
 // Gets the data about the director, by name
 
-app.get("/movies/director/:directorName", (req, res) => {
-    const author = movies.find(movie => movie.director.name === req.params.directorName).director;
-
-    if (author) {
-      res.status(200).json(author);
+app.get("/movies/director/:DirectorName", (req, res) => {
+  Movies.findOne({ "Director.Name": req.params.DirectorName})
+  .then((director) => {
+    if(director){
+      res.status(200).json(director.Director);
     } else {
-      res.status(400).send("Director with name " + req.params.name + " was not found.");
+      res.status(400).send(req.params.DirectorName + " was not found.")
     }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
 });
 
 // Allows new user to register
 /* We'll expect JSON in this format
 {
-  Username: String,
-  Password: String,
-  Email: String,
+  Username: String, (required)
+  Password: String, (required)
+  Email: String, (required)
   Birthday: Date
 }*/
 
@@ -192,11 +108,11 @@ app.post("/users", (req, res) => {
           Username: req.body.Username,
           Password: req.body.Password,
           Email: req.body.Email,
-          Birthday: req.body.Email
+          Birthday: req.body.Birthday
         })
         .then((user) => {res.status(201).json(user) })
       .catch((err) => {
-        console.error(error);
+        console.error(err);
         res.status(500).send("Error: " + err);
       })
     }
@@ -225,7 +141,11 @@ app.get("/users", (req, res) => {
 app.get("/users/:Username", (req, res) => {
   Users.findOne({ Username: req.params.Username })
     .then((user) => {
-      res.json(user);
+      if(user) {
+        res.status(200).json(user);
+      } else {
+        res.status(400).send(req.params.Username + " was not found.")
+      }
     })
     .catch((err) => {
       console.error(err);
@@ -264,7 +184,7 @@ app.put("/users/:Username", (req, res) => {
 
 // Add a movie to a user list of favorites
 
-app.post("/user/:Username/movies/:MovieID", (req, res) => {
+app.post("/users/:Username/movies/:MovieID", (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username },
   { $push: { FavoriteMovies: req.params.MovieID }},
   { new: true}, // This line makes sure that the updated document is returned
@@ -273,14 +193,14 @@ app.post("/user/:Username/movies/:MovieID", (req, res) => {
       console.error(err);
       res.status(500).send("Error: " + err);
     } else {
-      res.json(updatedUser);
+      res.send("The movie has been added succesfully");
     }
   });
 });
 
 // Removes a movie from a user list of favorites
 
-app.delete("/user/:Username/movies/:MovieID", (req, res) => {
+app.delete("/users/:Username/movies/:MovieID", (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username },
   { $pull: { FavoriteMovies: req.params.MovieID }},
   { new: true},
@@ -289,7 +209,7 @@ app.delete("/user/:Username/movies/:MovieID", (req, res) => {
       console.error(err);
       res.status(500).send("Error: " + err);
     } else {
-      res.json(updatedUser);
+      res.send("The movie has been removed succesfully");
     }
   });
 });
@@ -316,8 +236,6 @@ app.delete("/users/:Username", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Welcome to my Movies API!");
 });
-
-
 
 // Error handling
 
