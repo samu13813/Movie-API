d29f28fb012b6922b0fc42301549f0d95dfb9c7d
Sam Garcia <samuel.gsuero@gmail.com>
2022-04-07 09:48:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,17 +30,17 @@ app.use(express.static('public'));
 
 let allowedOrigins = ["http://localhost:1234"];
 
-const cors = require("cors");
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin is not found on the list of allowed origins
-      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// const cors = require("cors");
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin is not found on the list of allowed origins
+//       let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 // Imports auth to project.
 
